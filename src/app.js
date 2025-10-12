aa01e5beda69e471e39c6a91ccddb98c51b25ed5
Ashutosh <ashu57093@gmail.com>
2025-10-12 10:42:01 +0530

    Multiple Route Handler

--- src/app.js
+++ src/app.js
@@ -15,17 +15,18 @@ const app = express(); //calling a express function
 //     res.send("server is crashed");
 // })
 
-app.get("/abc", (req,res) =>{
-    console.log(req.query);
-    res.send({firstName: "Ashutosh",id:1234, city:"gzb"});
-})
-app.post("/about", (req,res) => {
-  res.send("this is a post call");
-});
+// app.get("/abc", (req,res) =>{
+//     console.log(req.query);
+//     console.log(request.params)
+//     res.send({firstName: "Ashutosh",id:1234, city:"gzb"});
+// })
+// app.post("/about", (req,res) => {
+//   res.send("this is a post call");
+// });
 
-app.use("/about",(req,res)=>{
-    res.send("this is a new")
-})
+// app.use("/about",(req,res)=>{
+//     res.send("this is a new")
+// })
 
 
 // get method use for routing
@@ -35,6 +36,34 @@ app.use("/about",(req,res)=>{
 // app.get("/about", (req, res) => {
 //   res.send("About Page");
 // });
+
+
+// handle multiple route handler inside the app.use method----
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("handler route 1");
+    // res.send("successful request 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("route handler 2");
+    // res.send("successful request 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("route handler 3");
+    // res.send("successful requset 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("route handler 4");
+    res.send("successful requset 4");
+  }
+);
+
+
 app.listen(5555,()=>{
     console.log("server is loading");
 });
